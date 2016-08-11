daff09f25b2c9c6b3a904e9b00c37d0ad637ff9b
León Ramos <leon.ramos@gmail.com>
2016-08-11 05:05:52 -0500

    Changed dialog boxes

--- programar_correo.sh
+++ programar_correo.sh
@@ -25,7 +25,7 @@ function null {
 dialog --textbox welcome.txt 22 70
 
 while [ "$CONT" != "yes" ] ; do 
-  MPATH=$(dialog --inputbox "Path to email body text file: " 8 40 --output-fd 1)
+  MPATH=$(dialog --title "Path to email body text file: " --fselect "" 8 40 --output-fd 1)
   null $MPATH
 done
 CONT="no"
@@ -50,67 +50,15 @@ while [ "$CONT" != "yes" ] ; do
 done
 CONT="no"
 while [ "$CONT" != "yes" ] ; do 
-  DAY=$(dialog --output-fd 1 --backtitle "Delivery Date" \
-  --radiolist "Select delivery day:" 38 40 31 \
-        1 1 on \
-        2 2 off \
-        3 3 off \
-        4 4 off \
-        5 5 off \
-        6 6 off \
-        7 7 off \
-        8 8 off \
-        9 9 off \
-        10 10 off \
-        11 11 off \
-        12 12 off \
-        13 13 off \
-        14 14 off \
-        15 15 off \
-        16 16 off \
-        17 17 off \
-        18 18 off \
-        19 19 off \
-        20 20 off \
-        21 21 off \
-        22 22 off \
-        23 23 off \
-        24 24 off \
-        25 25 off \
-        26 26 off \
-        27 27 off \
-        28 28 off \
-        29 29 off \
-        30 30 off \
-        31 31 off )
-  null $DAY
+  DATE=$(dialog --output-fd 1 --date-format %d.%m.%Y --calendar "Select delivery     date" 0 0)
+  null $DATE
 done
 CONT="no"
 while [ "$CONT" != "yes" ] ; do 
-  MONTH=$(dialog --output-fd 1 --backtitle "Delivery Date" \
-  --radiolist "Select delivery month:" 19 40 12 \
-        1 January on \
-        2 February off \
-        3 March off \
-        4 April off \
-        5 May off \
-        6 June off \
-        7 July off \
-        8 August off \
-        9 September off \
-        10 October off \
-        11 November off \
-        12 December off )
-  null $MONTH
+  TIME=$(dialog --output-fd 1 --time-format %H:%M --timebox "Select delivery time:" 0 0 )
+  null $TIME
 done
-CONT="no"
-while [ "$CONT" != "yes" ] ; do 
-  YEAR=$(dialog --output-fd 1 --backtitle "Delivery Date" \
-  --radiolist "Select delivery year:" 9 40 2 \
-        $CURRY Current on \
-        $NEXTY Next off )
-  null $YEAR
-done
+
 
 dialog --title "Attachment"  --yesno "Add attachment to e-mail?" 6 25
 if [ "$?" -eq "0" ] ; then
@@ -122,7 +70,7 @@ fi
 if [ "$ATTACH" == "yes" ] ; then
   CONT="no"
   while [ "$CONT" != "yes" ] ; do 
-    APATH=$(dialog --inputbox "Path to attachment file: " 8 40 --output-fd 1)
+    APATH=$(dialog --title "Path to attachment file: " --fselect "" 8 40 --output-fd 1)
     null $APATH
   done
 fi
@@ -134,7 +82,7 @@ echo "Sender's mail '$SMAIL'"
 echo "Sender's name '$SNAME'"
 echo "Destination mail '$DMAIL'"
 echo "Email subject '$SUBJECT'"
-echo "Delivery date (dd/mm/yyy): '$DAY/$MONTH/$YEAR'"
+echo "Delivery date (hh:min dd/mm/yyy): '$TIME $DATE'"
 if [ "$ATTACH" == "yes" ] ; then
   echo "Attachment '$APATH'"
 fi
@@ -143,7 +91,7 @@ EMAIL="\"$SNAME\" <$SMAIL>"
 echo "Email: '$EMAIL'"
 echo
 if [ "$ATTACH" == "no" ] ; then
-  echo "mutt -s \"$SUBJECT\" $DMAIL < $MPATH" | at 4:22 AM $DAY.$MONTH.$YEAR
+  echo "mutt -s \"$SUBJECT\" $DMAIL < $MPATH" | at $TIME $DATE
 else
-  mutt -s "$SUBJECT" -a $APATH -- $DMAIL < $MPATH | at 4:19 AM $DAY.$MONTH.$YEAR
+  echo "mutt -s \"$SUBJECT\" -a $APATH -- $DMAIL < $MPATH" | at $TIME $DATE
 fi
--- test.sh
+++ test.sh
@@ -1,36 +1,4 @@
 #!/bin/bash
 
-TEST=$(dialog --output-fd 1 --backtitle "Delivery Date" \
-  --radiolist "Select delivery day:" 38 40 31 \
-        1 1 on \
-        2 2 off \
-        3 3 off \
-        4 4 off \
-        5 5 off \
-        6 6 off \
-        7 7 off \
-        8 8 off \
-        9 9 off \
-        10 10 off \
-        11 11 off \
-        12 12 off \
-        13 13 off \
-        14 14 off \
-        15 15 off \
-        16 16 off \
-        17 17 off \
-        18 18 off \
-        19 19 off \
-        20 20 off \
-        21 21 off \
-        22 22 off \
-        23 23 off \
-        24 24 off \
-        25 25 off \
-        26 26 off \
-        27 27 off \
-        28 28 off \
-        29 29 off \
-        30 30 off \
-        31 31 off )
-echo "TEST '$TEST'"
+TEST=$(dialog --output-fd 1 --time-format %H:%M --timebox "Select delivery time:" 0 0  )
+ echo "$TEST"
